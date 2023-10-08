56cb1d0a9ed3ab5ee1044ab14da74bd77b327394
Yoosung-H <101240036+Hongyoosung@users.noreply.github.com>
2023-10-09 01:07:18 +0900

    2

--- Assets/my/BTNode/goalNode.cs
+++ Assets/my/BTNode/goalNode.cs
@@ -2,22 +2,14 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor;
-using UnityEngine.UIElements;
-using System.Collections.ObjectModel;
-
 
 public class goalNode : Action
 {
     private AgentGOAP selectedGoap;
     private List<GAction> availableActions = new List<GAction>();
-    private List<GameObject> objectList = new List<GameObject>();
-    private GameObject parentObject;
-    private Transform[] childTransforms;
 
     public string goal;
-    public string tag; 
+    public string tag;
 
     public AgentConsiderations[] StateVariables;
 
@@ -25,7 +17,6 @@ public class goalNode : Action
     {
         // Set the AgentConsiderations array in the GWorld instance
         GWorld.Instance.AgentConsiderations = StateVariables;
-
 
         // Get the first AgentGOAP component on the GameObject
         selectedGoap = this.gameObject.GetComponent<AgentGOAP>();
@@ -36,29 +27,22 @@ public class goalNode : Action
             return;
         }
 
-        // Find all game objects with 'Attack' tag and add their child objects to availableActions list.
-        parentObject = GameObject.FindGameObjectWithTag(tag);
-        childTransforms = parentObject.GetComponentsInChildren<Transform>(true);
+        // Find all game objects with 'tag' and add their GAction components to availableActions list.
+        GameObject[] parentObjects = GameObject.FindGameObjectsWithTag(tag);
 
-        foreach (Transform childTransform in childTransforms)
+        foreach (GameObject parentObject in parentObjects)
         {
-            if (childTransform != parentObject.transform)
-            {
-                GameObject childObject = childTransform.gameObject;
-                objectList.Add(childObject);
-            }
-        }
-
-
-        foreach (GameObject obj in objectList)
-        {
-            GAction[] actions = obj.GetComponents<GAction>();
+            GAction[] actions = parentObject.GetComponentsInChildren<GAction>();
             availableActions.AddRange(actions);
         }
     }
 
     public override TaskStatus OnUpdate()
     {
+        if (selectedGoap == null)
+        {
+            return TaskStatus.Failure;
+        }
 
         // Calculate utility for goals represented by their afterEffects
         foreach (AgentConsiderations ac in StateVariables)
@@ -66,20 +50,14 @@ public class goalNode : Action
             ac.UpdateConsideration();
         }
 
-
-        if (selectedGoap == null)
-        {
-            return TaskStatus.Failure;
-        }
-
         Debug.Log($"<color=#00FF00>GOAL: {goal} </color>");
 
         selectedGoap.CreateSubGoal(goal);
+
         selectedGoap.CarryOutPlan(availableActions);
+
+
+
         return TaskStatus.Success;
     }
 }
-
-
-
-
